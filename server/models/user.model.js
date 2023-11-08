7ccfd07c074a2ed8e8f69c8f520e325b58a6f585
jgonzalezsolis <jose_5lic@hotmail.com>
2023-11-08 11:31:05 -0700

    added backend login and reg

---
+++ server/models/user.model.js
@@ -0,0 +1,49 @@
+const mongoose = require ('mongoose')
+const bcrypt = require ('bcrypt')
+const { isEmail } = require ('validator')
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+    type: String,
+    required: [true, "First name is required"],
+    minlength: [2, "First name must be 2 characters or longer"]
+    },
+    lastName: {
+    type: String,
+    required: [true, "Last name is required"],
+    minlength: [2, "First name must be 2 characters or longer"]
+    },
+    email: {
+    type: String,
+    required: [true, "Email is required"]
+    },
+    password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [8, "Password must be 8 characters or longer"]
+    }
+}, 
+{timestamps: true}
+);
+
+UserSchema.virtual('confirmPassword')
+    .get( () => this._confirmPassword )
+    .set( value => this._confirmPassword = value );
+
+UserSchema.pre('validate', function(next) {
+    if (this.password !== this.confirmPassword) {
+        this.invalidate('confirmPassword', 'Passwords do not match, try again');
+    }
+    next();
+    });
+
+UserSchema.pre('save', function(next) {
+    bcrypt.hash(this.password, 10)
+        .then(hash => {
+        this.password = hash;
+        next();
+        });
+    });
+
+
+module.exports = mongoose.model("User", UserSchema)
--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 
-app.use(cors());
-app.use(express.json());                           
-app.use(express.urlencoded({ extended: true }));   
+const cors = require('cors');
+const cookieParser = require('cookie-parser')
+require ('dotenv').config()
 require('./config/mongoose.config');   
 require('./routes/gameposts.routes')(app);
+
+app.use(express.json());                           
+app.use(express.urlencoded({ extended: true }));   
+const userRoutes = require("./routes/user.routes");
+userRoutes(app);
+
+app.use(cors());
+app.use(cookieParser());
 app.listen(8000, () => {
     console.log("Listening at Port 8000")
 })
