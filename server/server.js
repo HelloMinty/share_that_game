100cdb93d740c05615cc161adb4b64cf0006d195
Antonio Clough <129467848+adclough12@users.noreply.github.com>
2023-11-15 17:33:39 -0600

    Update server.js
    Added socket,io

--- server/server.js
+++ server/server.js
@@ -1,24 +1,42 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const socket = require("socket.io");
 
-require ('dotenv').config()
+require ('dotenv').config();
 require('./config/mongoose.config');   
 
 app.use(cookieParser());
 app.use(express.json());                           
 app.use(express.urlencoded({ extended: true }));   
-app.use(cors({
-    credentials:true, 
-    origin:'http://localhost:5173'
-}));
+app.use(cors());
 
 const userRoutes = require("./routes/user.routes");
 userRoutes(app);
 require('./routes/gameposts.routes')(app);
 require('./routes/gamecomments.routes')(app);
 
-app.listen(8000, () => {
+const server = app.listen(8000, () => {
     console.log("Listening at Port 8000")
-})
+});
+
+const io = socket(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['*'],
+        credentials: true,
+    }
+});
+
+
+io.on("connection", socket => {
+    console.log('socket id: ' + socket.id);
+    
+    socket.on("event_from_client", data => {
+        // send a message with "data" to ALL clients EXCEPT for the one that emitted the
+    	//     "event_from_client" event
+        io.emit("event_to_all_clients", data);
+    });
+});
